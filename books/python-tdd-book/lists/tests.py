058878e627c7b744878af35e8ac3248f21773c0b
Matthew Reader <mdreader@gmail.com>
2021-01-28 14:08:15 -0500

    Moved to new repository

---
+++ books/python-tdd-book/lists/tests.py
@@ -0,0 +1,10 @@
+from django.urls import resolve
+from django.test import TestCase
+from lists.views import index
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve("/")
+        self.assertEqual(found.func, index)
