cb19a0c30e896c571fa59da820d351a3b9e9e668
Matthew Reader <mdreader@gmail.com>
2021-09-17 21:21:14 -0400

    Convert example to reactive expression
    Reactive expression minimize duplicated code.

--- books/mastering-shiny/chapter1/app.R
+++ books/mastering-shiny/chapter1/app.R
@@ -1,7 +1,24 @@
 library(shiny)
+
 ui <- fluidPage(
-  "Hello, world!"
+  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+  verbatimTextOutput("summary"),
+  tableOutput("table")
 )
+
 server <- function(input, output, session) {
+  # Create a reactive expression.
+  dataset <- reactive({
+    get(input$dataset, "package:datasets")
+  })
+  
+  output$summary <- renderPrint({
+    summary(dataset)
+  })
+  
+  output$table <- renderTable({
+    dataset()
+  })
 }
+
 shinyApp(ui, server)
