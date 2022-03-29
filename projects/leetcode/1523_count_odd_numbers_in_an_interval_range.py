58518daf71fe192af29b82dea12574c4b8b77fee
EC2 Default User <ec2-user@ip-172-31-23-58.ec2.internal>
2022-03-29 16:03:46 +0000

    Easy challenge solution

---
+++ projects/leetcode/1523_count_odd_numbers_in_an_interval_range.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        """
+        Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+        Args:
+            low: Lower boundary of the range
+            high: Upper boundary of the range
+        Returns:
+            int: Total odd numbers between low and high (inclusive)
+        """
+
+        # Two even endpoints will have half the range
+        if low % 2 == 0 and high % 2 == 0:
+            return (high - low) // 2
+            
+        # Any other endpoint will have half the range plus 1
+        else:
+            return ((high - low) // 2) + 1
