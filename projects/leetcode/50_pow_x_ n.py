22079ed26c672c32439c5436bac2c8af3c8ebc1d
matthewreader <mdreader@gmail.com>
2022-04-07 12:48:24 +0000

    Medium Leetcode problem

---
+++ projects/leetcode/50_pow_x_ n.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        """
+        Returns x ** n.  How is this a medium difficulty problem?
+
+        Args:
+            x: base as float
+        Returns:
+            n: exponent as int
+        """
+        
+        return x ** n
